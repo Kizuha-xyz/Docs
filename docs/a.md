3e5bf740a726a6328dfe9bf850887e10da3a83a0
Dominic Koppatz <dominickoppatz2003@gmail.com>
2021-06-30 18:41:56 +0200

    Update links

--- docs/a.md
+++ docs/a.md
@@ -1,5 +1,5 @@
 ---
-id: WelcomePage
+id: aWelcomePage
 title: Welcome
 ---
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/WelcomePage/')}>
+              to={useBaseUrl('docs/aWelcomePage/')}>
               View Documentation
             </Link>
           </div>
